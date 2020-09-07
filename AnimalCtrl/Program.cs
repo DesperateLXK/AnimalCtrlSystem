954372bcac24f78d78fab2b28d9f6324a64a06c4
LXK <lk960825@163.com>
2020-09-07 11:36:55 +0800

    first release

---
+++ AnimalCtrl/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibUsbDotNet;
+using LibUsbDotNet.Info;
+using LibUsbDotNet.Main;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+namespace AnimalCtrl
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            MainForm AnimalCtrlMainForm = new MainForm();
+            Application.Run(AnimalCtrlMainForm);
+        }
+        
+    }
+}
